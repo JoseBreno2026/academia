3b12dbf24f27e678a1dfa434c0a0782419cccc93
josealmeidatech <jose.almeida.tech@gmail.com>
2026-07-30 22:40:06 -0300

    feat: cria classe abstrata Pessoa

---
+++ src/main/java/br/edu/ufape/poo/academia/negocio/basico/Pessoa.java
@@ -0,0 +1,92 @@
+package br.edu.ufape.poo.academia.negocio.basico;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+public abstract class Pessoa {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private String cpf;
+	private String email;
+	private String telefone;
+	
+	// Construtor vazio
+    protected Pessoa() {
+    	super();
+    }
+
+    // Construtor com parâmetros
+    public Pessoa(String nome, String cpf, String email, String telefone) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
+    // Getters e Setters
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return this.cpf;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return this.telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cpf, email, id, nome, telefone);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+
+        Pessoa other = (Pessoa) obj;
+
+        return Objects.equals(cpf, other.cpf)
+                && Objects.equals(email, other.email)
+                && Objects.equals(id, other.id)
+                && Objects.equals(nome, other.nome)
+                && Objects.equals(telefone, other.telefone);
+    }
+}
