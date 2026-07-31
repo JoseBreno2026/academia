d454f3227dc69f2d3fe3be4e4cf1819083d3ec9f
josealmeidatech <jose.almeida.tech@gmail.com>
2026-07-31 17:38:13 -0300

    feat: adiciona classe TipoExercicio

---
+++ src/main/java/br/edu/ufape/poo/academia/negocio/basico/TipoExercicio.java
@@ -0,0 +1,78 @@
+package br.edu.ufape.poo.academia.negocio.basico;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Objects;
+
+@Entity
+public class TipoExercicio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String grupoMuscular;
+
+    // Construtor vazio
+    public TipoExercicio() {
+    	super();
+    }
+
+    // Construtor com parâmetros
+    public TipoExercicio(String nome, String grupoMuscular) {
+        this.nome = nome;
+        this.grupoMuscular = grupoMuscular;
+    }
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getGrupoMuscular() {
+        return grupoMuscular;
+    }
+
+    public void setGrupoMuscular(String grupoMuscular) {
+        this.grupoMuscular = grupoMuscular;
+    }
+
+
+	//HashCode - Equals
+	@Override
+	public int hashCode() {
+	    return Objects.hash(id, nome, grupoMuscular);
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+	    if (this == obj)
+	        return true;
+	    if (obj == null)
+	        return false;
+	    if (getClass() != obj.getClass())
+	        return false;
+	    
+	    TipoExercicio other = (TipoExercicio) obj;
+	    
+	    return Objects.equals(id, other.id) 
+	            && Objects.equals(nome, other.nome)
+	            && Objects.equals(grupoMuscular, other.grupoMuscular);
+	}
+}
