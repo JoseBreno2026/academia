9f3d9e567c233742a40bbab9d7716dfb38df7ece
JoseBreno2026 <josebrenoalmeida2018@gmail.com>
2026-07-31 02:44:49 -0300

    feat: envia projeto academia

---
+++ src/test/java/br/edu/ufape/poo/academia/AcademiaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ufape.poo.academia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
